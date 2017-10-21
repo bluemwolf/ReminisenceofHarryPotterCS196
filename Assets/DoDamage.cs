fa8fa68cc793a5033b5bb536743eca0d992d0b57
Jacqueline Osborn <jackieo5023@gmail.com>
2017-10-21 12:08:20 -0500

    Health Scripts

---
+++ Assets/DoDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//need to add this script to enemy for collision
+public class DoDamage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "insert name here")
+        {
+            collision.gameObject.GetComponent<Health>().LoseHealth(10); //some dmg
+        }
+    }
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//need to add script to player/object that has the health
+public class Health : MonoBehaviour {
+    public int maxHealth = 100;
+    public int currentHealth;
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth < 0)
+        {
+            gameObject.SetActive(false);
+            //somehow make it look like it dies or display a message
+        }
+	}
+    public void LoseHealth(int dmg)
+    {
+        currentHealth -= dmg;
+    }
+}
