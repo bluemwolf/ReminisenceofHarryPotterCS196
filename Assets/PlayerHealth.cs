f9224ff82a9a1477c400d7a0f53e49165644dfd6
Jacqueline Osborn <jackieo5023@gmail.com>
2017-10-29 12:00:29 -0500

    Added Health/Attack Scripts

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 //need to add script to player/object that has the health
-public class Health : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour {
     public int maxHealth = 100;
     public int currentHealth;
 	// Use this for initialization
@@ -12,14 +12,22 @@ public class Health : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (currentHealth < 0)
+		if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
             //somehow make it look like it dies or display a message
         }
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
 	}
-    public void LoseHealth(int dmg)
+    public void loseHealth(int dmg)
     {
         currentHealth -= dmg;
+    }
+    public void addHealth(int add)
+    {
+        currentHealth += add;
     }
 }
---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int[] playerHealthLevels; //can set these in unity
+    public int[] playerAttackLevels;
+
+    public int healthXP = 0;
+    public int currentPlayerMaxHealth;
+    public int currentPlayerHealthLevel = 0;
+
+    public int attackXP = 0;
+    public int currentPlayerAttack;
+    public int currentPlayerAttackLevel = 0;
+
+    private PlayerHealth health;
+
+	// Use this for initialization
+	void Start () {
+        health = FindObjectOfType<PlayerHealth>();
+        currentPlayerMaxHealth = playerHealthLevels[0];
+        currentPlayerAttack = playerAttackLevels[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (healthXP >= 50) // some number to level up health
+        {
+            levelHealthUp();
+        }
+        if (attackXP >= 50)
+        {
+            levelAttackUp();
+        }
+	}
+
+    public void levelHealthUp()
+    {
+        currentPlayerHealthLevel++;
+        currentPlayerMaxHealth = playerHealthLevels[currentPlayerHealthLevel];
+        health.maxHealth = currentPlayerMaxHealth;
+        health.currentHealth += currentPlayerMaxHealth - playerHealthLevels[currentPlayerHealthLevel];
+    }
+    public void levelAttackUp()
+    {
+        currentPlayerAttackLevel++;
+        currentPlayerAttack = playerAttackLevels[currentPlayerAttackLevel];
+    }
+}
