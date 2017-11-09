6d3a01bafbb064a117802677d40c0e5a8580bad8
Jacqueline Osborn <jackieo5023@gmail.com>
2017-11-09 13:31:23 -0600

    Added projectile shoot

---
+++ Assets/FireProjectileScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectileScript : MonoBehaviour {
+
+    public GameObject projectile;
+    public float speed;
+    private float maxSpeed = 5f;
+    private Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+     //   if (GetComponent<Rigidbody2D>().velocity.magnitude < maxSpeed)
+   //     {
+  //          GetComponent<Rigidbody2D>().AddForce(input * speed);
+ //       }
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            FireProjectile();
+        }
+	}
+
+    public void FireProjectile()
+    {
+         GameObject clone = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+         clone.GetComponent<Rigidbody2D>().velocity = (new Vector3(10, 0, 10));
+    }
+}
---
+++ Assets/ProjectileCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
