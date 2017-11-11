d48e628a7d9a3cbdd6de266cba1d329897429821
Jacqueline Osborn <jackieo5023@gmail.com>
2017-11-11 14:32:44 -0600

    Added projectile directions

--- Assets/FireProjectileScript.cs
+++ Assets/FireProjectileScript.cs
@@ -16,12 +16,7 @@ public class FireProjectileScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       // input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-     //   if (GetComponent<Rigidbody2D>().velocity.magnitude < maxSpeed)
-   //     {
-  //          GetComponent<Rigidbody2D>().AddForce(input * speed);
- //       }
-		if (Input.GetKeyDown(KeyCode.LeftShift))
+		if (Input.GetKeyDown(KeyCode.Space))
         {
             FireProjectile();
         }
@@ -29,7 +24,21 @@ public class FireProjectileScript : MonoBehaviour {
 
     public void FireProjectile()
     {
-         GameObject clone = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-         clone.GetComponent<Rigidbody2D>().velocity = (new Vector3(10, 0, 10));
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GameObject clone = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            clone.GetComponent<Rigidbody2D>().velocity = (new Vector3(10, 0, 10));
+            Destroy(clone, 2.0f);
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GameObject clone = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            clone.GetComponent<Rigidbody2D>().velocity = (new Vector3(-10, 0, 10));
+            Destroy(clone, 2.0f);
+        }
+        
+        //var shoot = (GameObject)Instantiate(projectile, this.transform.position + Vector3.Normalize(this.transform.forward) * 2f, this.transform.rotation);
+        //projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.forward * 30;
+        //Destroy(projectile, 2.0f);
+        //shootTime = Time.time + 1f / fireSpeed;
     }
 }
