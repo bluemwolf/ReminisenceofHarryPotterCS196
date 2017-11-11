638120c9e740c52d159352fda8800d9b94e6aa94
Justin Bieber <jzw2@gmail.com>
2017-11-11 15:33:17 -0600

    Please tell me what the error is
    k thx

---
+++ Assets/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+
+  void Start()
+  {
+      Button btn = GetComponent<Button>();
+      btn.onClick.AddListener(TaskOnClick);
+  }
+
+  void TaskOnClick() {
+	    SceneManager.LoadLevel("Some Scene");
+  }
+}
